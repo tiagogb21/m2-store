bead36cd1ad246e20c61d7a2107f3d54f3314aff
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-04 14:52:41 -0300

    <views><home> configurando homeviews

---
+++ m2-store/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <HomeProducts />
+  </div>
+</template>
+
+<script>
+import HomeProducts from '../components/home/HomeProducts.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    HomeProducts,
+  },
+};
+</script>
