1eee20b836c629a9c3394a35de8ee2999656d2e6
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-04 19:00:04 -0300

    <homeproducts> estruturando

--- m2-store/src/App.vue
+++ m2-store/src/App.vue
@@ -27,8 +27,6 @@ export default {
 }
 
 #app {
-  width: 95vw;
-  height: 95vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- m2-store/src/pages/home/HomeProducts.vue
+++ m2-store/src/pages/home/HomeProducts.vue
@@ -1,28 +1,49 @@
 <template>
-  <main>
+  <main class="container">
+    <article class="total__box">
+      <p>Total</p>
+      <p>R$ /mês</p>
+    </article>
+    <article class="instalacao__box">
+      <p>Taxa de instalação</p>
+      <p>Grátis</p>
+    </article>
     <section
-      class="product_container"
+      class="product__container"
       v-for="product in productList"
       v-bind:key="product.id"
     >
-      <h3>{{ product }}</h3>
-      <article v-if="product === 'Fixo'">
-        <p>{{ productFixo }}</p>
-      </article>
-      <article v-else-if="product === 'TV'">
-        <article v-for="prod in productTV" v-bind:key="prod.id">
-          <p>{{ prod }}</p>
+      <h3 class="title">{{ product }}</h3>
+      <article v-if="product === 'Fixo'" class="fixo__container">
+        <h4>Agora escolha seu pacote de telefone fixo</h4>
+        <article class="fixo__item">
+          <p>{{ productFixo }}</p>
         </article>
       </article>
-      <article v-else class="tv__container">
-        <p>Selecione um plano de internet para continuar</p>
+      <article v-else-if="product === 'TV'">
+        <h4>Agora escolha seu pacote de televisão</h4>
         <article class="tv__box">
           <article
-            v-for="prod in productInternet"
+            v-for="prod in productTV"
             v-bind:key="prod.id"
             class="tv__item"
           >
             <p>{{ prod }}</p>
+          </article>
+        </article>
+      </article>
+      <article v-else class="internet__container">
+        <h4>Selecione um plano de internet para continuar</h4>
+        <article class="internet__box">
+          <article
+            v-for="prod in productInternet"
+            v-bind:key="prod.id"
+            class="internet__item"
+          >
+            <input type="radio" name="input-internet" id="input-internet" />
+            <p>{{ prod }}</p>
+            <p>R$</p>
+            <p>+ Detalhes</p>
           </article>
         </article>
       </article>
@@ -54,16 +75,73 @@ export default {
 </script>
 
 <style scoped>
-.tv__container {
+.container {
+  width: 70vw;
   display: flex;
-  flex-direction: column;
+  flex-direction: column-reverse;
 }
+
+.title {
+  text-transform: uppercase;
+  color: rgb(185, 147, 210, 1);
+}
+
+.fixo__item {
+  display: flex;
+  width: 50%;
+  border: 1px solid black;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+}
+
+.fixo__item:hover,
+.tv__item:hover,
+.internet__item:hover {
+  cursor: pointer;
+  color: white;
+  border: none;
+  background: rgb(185, 147, 210, 1);
+}
+
 .tv__box {
   display: flex;
   justify-content: space-around;
 }
+
 .tv__item {
-  display: flex;
+  width: 30%;
   border: 1px solid black;
+  text-align: center;
+}
+
+.internet__container {
+  display: flex;
+  flex-direction: column;
+}
+
+.internet__box {
+  display: flex;
+  justify-content: space-around;
+}
+
+.internet__item {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  justify-content: center;
+  align-items: center;
+}
+
+.instalacao__box,
+.total__box {
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.total__box {
+  color: rgb(185, 147, 210, 1);
 }
 </style>
